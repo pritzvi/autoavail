5c5cc94791736775d02528e18c1a6788e3d97a89
pritzvi <prithvi.patre03@gmail.com>
2025-05-12 14:15:08 -0400

    updated to add some openai instructions

--- autoavail/config.js
+++ autoavail/config.js
@@ -2,6 +2,6 @@
 window.config = {
   BACKEND_API_URL: "http://localhost:3000",
   OPENAI_API_KEY: "", 
-  GOOGLE_CLIENT_ID: "887067579675-s0ufthrpkrsb8ppua4l9jlsk9itnahfs.apps.googleusercontent.com",
+  GOOGLE_CLIENT_ID: "109759730100-odbl1bujv808prki6ip3e471e5v37g17.apps.googleusercontent.com",
   GOOGLE_REDIRECT_URI: "http://localhost:3000/api/auth/callback/google"
 }; 
